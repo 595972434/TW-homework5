bf9a5f715ebc950c96424255a237a55cdf7a856b
Cas <595972434@qq.com>
2019-02-26 14:52:18 +0800

    样例1：指定菜品减半 样例通过正常

--- spec/best-charge-spec.js
+++ spec/best-charge-spec.js
@@ -1,6 +1,6 @@
 const bestCharge = require('../src/best-charge');
 describe('Take out food', function () {
-/*
+
   it('should generate best charge when best is 指定菜品半价', function() {
     let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
     let summary = bestCharge(inputs).trim();
@@ -17,7 +17,7 @@ describe('Take out food', function () {
 ===================================`.trim()
     expect(summary).toEqual(expected)
   });
-*/
+
   it('should generate best charge when best is 满30减6元', function() {
     let inputs = ["ITEM0013 x 4", "ITEM0022 x 1"];
     let summary = bestCharge(inputs).trim();
